3f14b324132c16de0d1625d8323ac4204fb41b2b
Elias Goldmann Brandtzæg <eliasgb@ulrik.uio.no>
2023-09-07 15:37:27 +0200

    changes in description

--- shorten.js
+++ shorten.js
@@ -1,17 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
-    console.log("Script is running");
     var projectDescriptions = document.querySelectorAll(".project-description");
 
-    // Define the word limit
     var wordLimit = 30;
 
-    // Iterate through each project description
     projectDescriptions.forEach(function (description) {
         var words = description.textContent.split(" ");
         if (words.length > wordLimit) {
-            // Truncate the text to the word limit
             description.textContent = words.slice(0, wordLimit).join(" ") + " ...";
-            console.log("Short done");
         }
     });
 });

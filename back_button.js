2ad111ea7f625d0c1125489a147999875f28b272
Elias Goldmann Brandtzæg <eliasgb@ulrik.uio.no>
2023-09-08 12:12:51 +0200

    New back button

---
+++ back_button.js
@@ -0,0 +1,11 @@
+function scrollToSection() {
+    const hash = window.location.hash;
+    if (hash) {
+        const target = document.querySelector(hash);
+        if (target) {
+            target.scrollIntoView({ behavior: 'smooth' });
+        }
+    }
+}
+
+window.addEventListener('load', scrollToSection);

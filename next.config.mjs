89d90ab3ac3bf602119317741cdbbdc8a538b542
eliasgb <eliasgb@ulrik.uio.no>
2024-03-11 01:45:53 +0100

    Changed path for img

--- next.config.mjs
+++ next.config.mjs
@@ -3,10 +3,6 @@ const nextConfig = {
     basePath: '/eliasgoldmann',
     assetPrefix: '/eliasgoldmann/',
     output: "export",
-    images: {
-        loader: "akamai",
-        path: "/",
-    },
 };
 
 export default nextConfig;
